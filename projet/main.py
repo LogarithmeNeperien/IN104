f44dd698d6dd99cfdb155d745725b846ca1d450c
Cengiz Theophane <theophane.cengiz77@gmail.com>
2021-05-03 16:49:10 +0200

    Now running a nice choregraphy

--- projet/main.py
+++ projet/main.py
@@ -9,22 +9,40 @@ from simulator.graphics import Screen
 import pygame as pg
 
 if __name__ == "__main__":
-    b1 = Body(Vector2(0, 0),
-              velocity=Vector2(0, 0),
-              mass=10,
-              draw_radius=10)
-    b2 = Body(Vector2(1, 1),
-              velocity=Vector2(0, 0.2),
-              mass=1,
-              draw_radius=5)
 
+    v1 = 0.3471168881
+    v2 = 0.5327249454
+    M = 1
+
+    b1 = Body(Vector2(-1,0),
+              velocity=Vector2(v1, v2),
+              mass=M,
+              color = (0,128,128),
+              draw_radius=2)
+    b2 = Body(Vector2(1, 0),
+              velocity=Vector2(v1, v2),
+              mass=M,
+              color = (255,240,20),
+              draw_radius=2)
+    b3 = Body(Vector2(0,0),
+              velocity = Vector2(-2*v1,-2*v2),
+              mass = M,
+              color = (255,50,50),
+              draw_radius =2)
+    # b4 = Body(Vector2(15, 15),
+    #           velocity=Vector2(-3, 3),
+    #           mass=300,
+    #           color = (255,50,50),
+    #           draw_radius=20)
     world = World()
     world.add(b1)
     world.add(b2)
+    world.add(b3)
+    #world.add(b4)
 
     simulator = Simulator(world, DummyEngine, DummySolver)
 
-    screen_size = Vector2(800, 600)
+    screen_size = Vector2(900, 600)
     screen = Screen(screen_size,
                     bg_color=(0, 0, 0),
                     caption="Simulator")
@@ -32,7 +50,7 @@ if __name__ == "__main__":
 
     # this coefficient controls the speed
     # of the simulation
-    time_scale = 10
+    time_scale = 1000
 
     print("Start program")
     while not screen.should_quit:
@@ -48,8 +66,10 @@ if __name__ == "__main__":
         # handle events
         #   scroll wheel
         if screen.get_wheel_up():
+            screen.erase()
             screen.camera.scale *= 1.1
         elif screen.get_wheel_down():
+            screen.erase()
             screen.camera.scale *= 0.9
 
         # draw current state
