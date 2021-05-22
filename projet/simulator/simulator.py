ac84ab63c0041ca98d215c19fb0763e9c0303084
Cengiz Theophane <theophane.cengiz77@gmail.com>
2021-05-22 19:02:01 +0200

    oublie d'import de Vector pour LeapFrogSolver

--- projet/simulator/simulator.py
+++ projet/simulator/simulator.py
@@ -1,4 +1,8 @@
-#11/05 Seta: correction de la méthode step, erreur d'indexation dans y pour velocity 
+#11/05 Seta: correction de la méthode step, erreur d'indexation dans y pour velocity
+
+from .utils.vector import Vector, Vector2
+from .solvers import DummySolver, LeapFrogSolver
+
 class Simulator:
     def __init__(self, world, Engine, Solver):
         self.t = 0
@@ -15,22 +19,25 @@ class Simulator:
         # and velocities of the bodies) as a Vector
 
         y0 = self.engine.make_solver_state()
+        a0 = Vector(int(len(y0)/2))
+        if Solver == LeapFrogSolver :
+            self.solver = Solver(self.engine.derivatives, self.t, y0, a0)
+        else :
+            self.solver = Solver(self.engine.derivatives, self.t, y0)
 
-        self.solver = Solver(self.engine.derivatives, self.t, y0)
+    def step(self, h):
 
-    def step(self, h):       
-                        
-        y = self.solver.integrate(self.t + h)      
-            
-            
+        y = self.solver.integrate(self.t + h)
+
+
         for i in range(len(self.world)):
             b_i = self.world.get(i)
 
             b_i.position.set_x(y[2 * i])
             b_i.position.set_y(y[2 * i + 1])
-        
+
             b_i.velocity.set_x(y[2*len(self.world) + 2 * i])
-            
+
             b_i.velocity.set_y(y[2*len(self.world) + 2 * i + 1])
 
         self.t += h
